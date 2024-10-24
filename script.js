14844c0ad3dad14314498131be39f8dd2e616530
ejbau <ejbautista0209@gmail.com>
2024-10-24 18:21:29 +0800

    Initial commit with files

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("changeText").addEventListener("click", function() {
+    document.getElementById("content").innerHTML = `
+        <h2>Thank You!</h2>
+        <p>You clicked the button and the content has changed!</p>
+    `;
+});
